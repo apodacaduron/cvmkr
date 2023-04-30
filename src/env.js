b34144e6fbcf07dab8cc1bd78fb6aea942b8255f
Daniel Apodaca <apodacaduron@gmail.com>
2023-04-29 20:30:41 -0600

    remove mjs files

---
+++ src/env.js
@@ -0,0 +1,38 @@
+// @ts-check
+/**
+ * This file is included in `/next.config.js` which ensures the app isn't built with invalid env vars.
+ * It has to be a `.js`-file to be imported there.
+ */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { z } = require("zod");
+
+/*eslint sort-keys: "error"*/
+const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
+  NEXTAUTH_SECRET:
+    process.env.NODE_ENV === "production"
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
+  NEXTAUTH_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    (str) => process.env.VERCEL_URL ?? str,
+    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    process.env.VERCEL ? z.string().min(1) : z.string().url()
+  ),
+  NODE_ENV: z.enum(["development", "test", "production"]),
+});
+
+const env = envSchema.safeParse(process.env);
+
+if (!env.success) {
+  console.error(
+    "❌ Invalid environment variables:",
+    JSON.stringify(env.error.format(), null, 4)
+  );
+  process.exit(1);
+}
+module.exports.env = env.data;
